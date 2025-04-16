acfb9b837614fcc0547957e368b517a369974229
Yori Geovanni <zlock88@gmail.com>
2025-04-16 15:25:26 +0900

    restrcuture

--- app/routes/aplikasi+/internal+/amc+/index.tsx
+++ app/routes/aplikasi+/internal+/amc+/index.tsx
@@ -1,14 +1,10 @@
 
-export default function AplikasiAmc (){
-  return (
-    <div className="min-h-screen bg-gray-50 dark:bg-slate-950">
-     
-        <div className="w-full  p-4">
-          <h1>AMC Page</h1>
-          <p>Welcome to the AMC page!</p>
+export default () =>{
+    return (
+        <div className="flex flex-col w-full h-full px-4 py-4 overflow-hidden">
+            <div className="flex items-center justify-between w-full h-12 px-2 bg-white border-b border-gray-300">
+                <h1 className="text-lg font-bold">DATA AIRPORT</h1>
+            </div>
         </div>
-    
-    </div>
-  );
-};
-
+    )
+}
