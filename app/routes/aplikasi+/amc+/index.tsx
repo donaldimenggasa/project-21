730acdc82c2983181b9390d4147e1f516c47da52
donaldimenggasa <donaldimenggasa@gmail.com>
2025-04-16 15:15:37 +0900

    update tampilan

--- app/routes/aplikasi+/amc+/index.tsx
+++ app/routes/aplikasi+/amc+/index.tsx
@@ -1,4 +1,10 @@
 
 export default () =>{
-    return (<>NOT FOUND 404</>)
+    return (
+        <div className="flex flex-col w-full h-full px-4 py-4 overflow-hidden">
+            <div className="flex items-center justify-between w-full h-12 px-2 bg-white border-b border-gray-300">
+                <h1 className="text-lg font-bold">DATA AIRPORT</h1>
+            </div>
+        </div>
+    )
 }
